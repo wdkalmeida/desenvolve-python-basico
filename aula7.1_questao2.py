b3c0460f9724f028d07c95d6fffdcaecc7510dc0
wdkalmeida <166380472+wdkalmeida@users.noreply.github.com>
2025-01-17 00:03:28 -0300

    Add files via upload
    Exercicios do modulo 7

---
+++ aula7.1_questao2.py
@@ -0,0 +1,8 @@
+#solicite o primeiro nome do usuario
+nome = input("Digite seu primeiro nome: ")
+
+#solicite o sobrenome do usuario
+sobrenome = input("Digite seu sobrenome: ")
+
+#concatenando as duas strings e exibindo uma mensagem de boas vindas
+print("Bem-vindo(a), " + nome + " " + sobrenome + "!")
