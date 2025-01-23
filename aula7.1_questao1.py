86ef32cb45db8cebc923478cdb6df367963f63c0
wdkalmeida <166380472+wdkalmeida@users.noreply.github.com>
2025-01-23 18:27:29 -0300

    Delete aula7.1_questao1.py

--- aula7.1_questao1.py
+++
@@ -1,8 +0,0 @@
-#Solicita o nome do usuario
-nome = input("Digite seu nome: ")
-
-#Itera sobre o tamanho do nome para criar a escada
-#itera uma letra por vez e vai acrescentando na escada
-for i in range(1,len(nome) + 1):
-    print((nome[:i]))
-          
